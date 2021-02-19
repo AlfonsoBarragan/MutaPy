21f0d1aab9201eaaf73c99e2619d711446cff4a2
Alfonso <32295133+AlfonsoBarragan@users.noreply.github.com>
2021-02-19 01:30:22 +0100

    Implemented (again) Symbiotic Organisms Search Algorithm and the main file to check that works correctly before make a more intense testing

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import section
+
+import sos_algorithm
+
+# Declaration section
+
+    #TODO
+
+if __name__ == '__main__':
+
+    print('campo de pruebas')
+
+    # TODO implementacion de ejemplos para ver que tal va
